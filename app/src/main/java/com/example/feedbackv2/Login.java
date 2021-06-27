9825a354be76e1829ea8bea8379f488368115b63
bumblebee12367 <77162830+bumblebee12367@users.noreply.github.com>
2021-06-28 01:00:15 +0300

    Login page event
    Added an event on login page that redirect the user to Register page to create an account by clicking on the "New user?..." TextView

---
+++ app/src/main/java/com/example/feedbackv2/Login.java
@@ -0,0 +1,36 @@
+package com.example.feedbackv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+Button mLogin;
+TextView mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_login);
+
+        mRegister=(TextView)findViewById(R.id.registertext);
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Register.class));
+                finish();
+            }
+        });
+
+    }
+}
