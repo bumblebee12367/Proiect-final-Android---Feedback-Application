51abfa55d8b632247283187d01d8ffeff88ddd5f
bumblebee12367 <77162830+bumblebee12367@users.noreply.github.com>
2021-06-29 23:47:09 +0300

    Update Profile.java

--- app/src/main/java/com/example/feedbackv2/Profile.java
+++ app/src/main/java/com/example/feedbackv2/Profile.java
@@ -1,5 +1,6 @@
 package com.example.feedbackv2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,13 +11,22 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.CDATASection;
 
 public class Profile extends AppCompatActivity {
-TextView mName, mEmail;
+TextView mName, mEmail,mAge,mSubject;
 FirebaseAuth fAuth;
 Button mLogout;
+DatabaseReference mDataBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,9 +37,12 @@ Button mLogout;
 
         setContentView(R.layout.activity_profile);
         fAuth=FirebaseAuth.getInstance();
+        mAge=findViewById(R.id.profile_age);
+        mSubject=findViewById(R.id.profile_subject);
         mName=findViewById(R.id.profile_name);
         mEmail=findViewById(R.id.profile_email);
         mLogout=findViewById(R.id.logout);
+
         mLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +60,32 @@ Button mLogout;
         FirebaseUser fUser= fAuth.getCurrentUser();
         mEmail.setText(fUser.getEmail());
         mName.setText(fUser.getDisplayName());
+
+
+        //FirebaseUser fUser=FirebaseAuth.getInstance(FirebaseApp.getInstance("https://feedback-9a043-default-rtdb.europe-west1.firebasedatabase.app/")).getCurrentUser();
+        //String key = mDataBase.push().getKey();
+
+        //mDataBase = FirebaseDatabase.getInstance("https://feedback-9a043-default-rtdb.europe-west1.firebasedatabase.app/").getReference().child(fUser.getUid());
+       // mDataBase.addValueEventListener(new ValueEventListener() {
+         //   @Override
+          //  public void onDataChange(@NonNull DataSnapshot snapshot) {
+           //     String name = snapshot.child("name").getValue().toString();
+          //      String email = snapshot.child("email").getValue().toString();
+          //      String age = snapshot.child("age").getValue().toString();
+         //       String subject = snapshot.child("subject").getValue().toString();
+
+         //       mName.setText(name);
+           //     mEmail.setText(email);
+           //     mAge.setText(age);
+           //     mSubject.setText(subject);
+           // }
+
+         //   @Override
+         //   public void onCancelled(@NonNull DatabaseError error) {
+
+          //  }
+      //  });
     }
 
 
-}
+    }
